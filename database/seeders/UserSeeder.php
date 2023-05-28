f8f8298e6586ba3d1b63e27d16f067f4ab54b41f
rayhan52ve <sajidrayhan875@gmail.com>
2023-05-28 21:50:42 +0600

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Str;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+       $user = [
+        'name'=>'Sajid Rayhan',
+        'role'=>2,
+        'email'=>'admin@gmail.com',
+        'password'=> bcrypt(123456)
+       ];
+       User::create($user);
+    }
+}
