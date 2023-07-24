b8df5a873ffbe1b96fd749f6c4fc43792e8b2b6f
rayhan52ve <sajidrayhan875@gmail.com>
2023-07-25 00:14:28 +0600

    factoryu & databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        // \App\Models\Event::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            EventSeeder::class,
+        ]);
+
     }
 }
